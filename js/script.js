6ccc4098d02c6cb7d306f4f771da9da6d996c081
lidner <lidner.ls@gmail.com>
2021-11-25 18:47:51 +0600

    Class based menu cards

--- js/script.js
+++ js/script.js
@@ -1,42 +1,99 @@
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function () {
 
     // Tabs
     let tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
         tabsParent = document.querySelector('.tabheader__items');
 
-    
-        function hideTabContent() {
-            tabsContent.forEach(item => {
-               item.classList.add('hide');
-               item.classList.remove('show');
-            });
 
-            tabs.forEach(item => {
-                item.classList.remove('tabheader__item_active');
-            });
-        }
-
-        function showTabContent(i = 0) {
-            tabsContent[i].classList.add('show', 'fade');
-            tabsContent[i].classList.remove('hide');
-            tabs[i].classList.add('tabheader__item_active');
-        }
-
-
-        hideTabContent();
-        showTabContent();
-
-        tabsParent.addEventListener('click', (e) => {
-            const target = e.target;
-
-            if (target && target.classList.contains('tabheader__item')) {
-                tabs.forEach((item, i) => {
-                    if (target == item) {
-                        hideTabContent();
-                        showTabContent(i);
-                    }
-                });
-            }
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show');
         });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (e) => {
+        const target = e.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+
+    // Class based menu cards 
+    class MenuCard {
+        constructor(src, alt, title, price, desc, parentSelector) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.price = price;
+            this.desc = desc;
+            this.parent = document.querySelector(parentSelector);
+        }
+
+        render() {
+            const element = document.createElement('div');
+            element.innerHTML = `
+                    <div class = "menu__item">
+                        <img src = "${this.src}" alt = "${this.alt}">
+                        <h3 class = "menu__item-subtitle">${this.title}</h3>
+                        <div class = "menu__item-descr">${this.desc}</div>
+                        <div class = "menu__item-divider"></div>
+                        <div class = "menu__item-price">
+                            <div class = "menu__item-cost" > Цена: </div>
+                            <div class = "menu__item-total"><span>${this.price}</span> грн/день </div>
+                        </div>
+                    </div>
+                `
+            this.parent.append(element);
+        }
+    }
+
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        111,
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        '.menu .container'
+    ).render();
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню "Премиум"',
+        222,
+        'Меню "Премиум" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        '.menu .container'
+    ).render();
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        333,
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        '.menu .container'
+    ).render();
 });
