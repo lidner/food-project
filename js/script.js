6825f604e533541ff2d62e9a89c7ceed2c506dad
lidner <lidner.ls@gmail.com>
2021-11-30 16:09:50 +0600

    Modal modified by scroll event

--- js/script.js
+++ js/script.js
@@ -47,11 +47,14 @@ window.addEventListener('DOMContentLoaded', function () {
           modalClose = document.querySelector('[data-close]');
     
     modalTrigger.forEach(btn => {
-        btn.addEventListener('click', () => {
-            modal.classList.toggle('show');
-            document.body.style.overflow = 'hidden';
-        });
+        btn.addEventListener('click', openModal);
     });
+
+    function openModal() {
+        modal.classList.toggle('show');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
+    }
 
     function closeModal() {
         modal.classList.toggle('show');
@@ -71,6 +74,18 @@ window.addEventListener('DOMContentLoaded', function () {
             closeModal();
         }
     });
+
+    const modalTimerId = setTimeout(openModal, 5000);
+
+
+    function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
 
     // Class based menu cards 
     class MenuCard {
